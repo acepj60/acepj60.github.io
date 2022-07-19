13e2620867f1b0dac5c694a458ba61c061012f60
acepj60 <52097845+acepj60@users.noreply.github.com>
2022-07-19 23:59:35 +0300

    Create whois.php

---
+++ whois.php
@@ -0,0 +1,80 @@
+
+<html>
+<head>
+<title>[+] Whois Lookup [+]</title>
+<meta name="viewport" content="width=device-width, initial-scale=1" /> 
+  <script src="https://cdn.jsdelivr.net/npm/pace-js@latest/pace.min.js"></script>
+  <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/pace-js@latest/pace-theme-default.min.css">
+</head>
+<body bgcolor="#1e1e1e" text="white">
+<center>
+  <font size=5>Whois Lookup</font><br><font size="3">find the registered owner, netblock, ASN and registration dates</font><hr>
+  <style>
+  @import url('https://fonts.googleapis.com/css?family=Staatliches');
+  @import url('https://fonts.googleapis.com/css?family=Roboto+Mono');
+
+textarea {
+max-width: 90%;
+width: 100%;
+height: 150px;
+resize: none;
+outline: none;
+overflow: hidden;
+background: transparent;
+color: #ffffff;
+text-align: center;
+}
+
+pre {
+  text-align: left;
+}
+
+button {
+  background: transparent;
+    font-family: Staatliches;
+  color: #ffffff;
+  border-color: #ffffff;
+  cursor: pointer;
+}
+
+select {
+  background: transparent;
+  color: #ffffff;
+}
+
+select option {
+  background-color: #1e1e1e}
+input {
+  background: transparent;
+  font-family: Staatliches;
+  color: #ffffff;
+  border-color: #ffffff;
+  cursor: pointer;
+}
+
+font {
+  font-family: Staatliches;
+}
+
+body::-webkit-scrollbar {
+  width: 12px;               /* width of the entire scrollbar */
+}
+
+body::-webkit-scrollbar-track {
+  background: #1e1e1e;        /* color of the tracking area */
+}
+
+body::-webkit-scrollbar-thumb {
+  background-color: #1e1e1e;    /* color of the scroll thumb */
+  border: 3px solid gray;  /* creates padding around scroll thumb */
+}
+
+oren {
+  color: orange;
+}
+</style>
+
+<center><form method="post">
+<input type="text" name="url" height="20" placeholder="https://exploits.my.id" style="padding-left: 5px; margin: 10px;font-family: Roboto Mono" size="40" required></textarea><br>
+<input type="submit" name="go" value="Whois" onclick="ngeklik()">
+<hr><font size=3>Unknown45 - 2021
